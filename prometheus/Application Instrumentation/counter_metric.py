fd23c6dd740cad0fcaf121252ddb4ea0262086c0
Samuel Awonusi <samuel.awonusi@engineering.digital.dwp.gov.uk>
2023-04-05 13:05:45 +0100

    Changed the localhost to the ip address of the server in the count_metric.py file

--- prometheus/Application Instrumentation/counter_metric.py
+++ prometheus/Application Instrumentation/counter_metric.py
@@ -1,13 +1,12 @@
 import http.server
 from prometheus_client import start_http_server, Counter
 
-REQUEST_COUNT = Counter("app_request_counts", "Total HTTP Request Count", ['python_custom_app','endpoint'])
+REQUEST_COUNT = Counter("app_request_counts", "Total HTTP Request Count")
 
 class HandleRequests(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
-        #REQUEST_COUNT.inc()
-        REQUEST_COUNT.labels('get_funcation', self.path).inc()
+        REQUEST_COUNT.inc()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
@@ -16,5 +15,5 @@ class HandleRequests(http.server.BaseHTTPRequestHandler):
 
 if __name__ == "__main__":
     start_http_server(5001)
-    server = http.server.HTTPServer(('178.62.224.102', 5000), HandleRequests)
+    server = http.server.HTTPServer(('46.101.171.233', 5000), HandleRequests)
     server.serve_forever()
